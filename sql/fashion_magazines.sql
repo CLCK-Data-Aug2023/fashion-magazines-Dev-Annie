50b4248b8ce4b54c23e8fe6bb02ae41870543cc4
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2024-02-18 21:52:28 -0500

    added fashion magazine as type

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,5 +1,4 @@
 SELECT 
-    customers.customer_id AS CustomerID,
     customers.customer_name AS Customer,
     PRINTF('$%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS total_amount_due
 FROM 
@@ -9,6 +8,6 @@ JOIN
 JOIN 
     subscriptions ON orders.subscription_id = subscriptions.subscription_id
 WHERE 
-    orders.order_status = 'unpaid'
+    subscriptions.description = 'Fashion Magazine' AND orders.order_status = 'unpaid'
 GROUP BY 
-    customers.customer_id, customers.customer_name;
+    customers.customer_name;
