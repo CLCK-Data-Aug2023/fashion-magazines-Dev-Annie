604009142b3849f5cc0b1328360517a75167c492
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2024-02-18 21:29:49 -0500

    grouped by name

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,14 +1,14 @@
-SELECT
-    orders.order_id,
+SELECT 
+    customers.customer_id,
     customers.customer_name,
     PRINTF('$%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS total_amount_due
-FROM
+FROM 
     orders
-JOIN
+JOIN 
     customers ON orders.customer_id = customers.customer_id
-JOIN
+JOIN 
     subscriptions ON orders.subscription_id = subscriptions.subscription_id
-WHERE
+WHERE 
     orders.order_status = 'unpaid'
-GROUP BY
-    customers.customer_name;
+GROUP BY 
+    customers.customer_id, customers.customer_name;
