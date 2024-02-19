07662f17dad4cc762705ed87cce68932e425f865
Dev-Annie <85464723+Dev-Annie@users.noreply.github.com>
2024-02-18 22:07:16 -0500

    changed headers again

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,13 +1,17 @@
 SELECT 
-    customers.customer_name AS Customer,
-    PRINTF('$%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS total_amount_due
+    customers.customer_id,
+    customers.customer_name AS "Customer",
+    PRINTF('$%.2f', SUM(subscriptions.price_per_month * subscriptions.subscription_length)) AS "Amount Due"
 FROM 
     orders
 JOIN 
-    customers ON orders.customer_id = customers.customer_id
+    customers 
+    ON orders.customer_id = customers.customer_id
 JOIN 
-    subscriptions ON orders.subscription_id = subscriptions.subscription_id
+    subscriptions 
+    ON orders.subscription_id = subscriptions.subscription_id
 WHERE 
-    subscriptions.description = 'Fashion Magazine' AND orders.order_status = 'unpaid'
+    subscriptions.description = 'Fashion Magazine' 
+    AND orders.order_status = 'unpaid'
 GROUP BY 
     customers.customer_name;
